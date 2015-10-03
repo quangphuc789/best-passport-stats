6441a80c68fe88d7a2782a04e2d0bfeee23f5dde
qp <qp@powerme.com.sg>
2015-10-04 03:25:21 +0800

    modified setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,2 +1,5 @@
 sudo aptitude install apache2 apache2.2-common apache2-mpm-prefork apache2-utils libexpat1 ssl-cert
 sudo aptitude install libapache2-mod-wsgi
+# Make get-pip.py https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+pip install Django
